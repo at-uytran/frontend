312415d9187860125613cb8d4d8b176efdb7794d
at-uytran <vanuy113@gmail.com>
2019-08-18 14:26:01 +0700

    add modules

---
+++ src/app/product/product.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product.component';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { NewProductComponent } from './new-product/new-product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+
+
+const routes: Routes = [
+  {
+    path: 'product',
+    component: ProductComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full'
+      },
+      {
+        path: 'list',
+        component: ProductListComponent
+      },
+      {
+        path: 'new',
+        component: NewProductComponent
+      },
+      {
+        path: 'detail',
+        component: ProductDetailComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  declarations: [ProductComponent, ProductListComponent, NewProductComponent, ProductDetailComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class ProductModule { }
